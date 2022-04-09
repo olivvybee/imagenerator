07126ccd7bb14012a4eb026078deca7573a07dca
Olivia Asch <olivia.c.asch@gmail.com>
2022-04-09 17:06:45 +0100

    Add a navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useRef, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import classNames from 'classnames';
+import _sortBy from 'lodash/sortBy';
+
+import { generators } from '../../generators';
+
+import buttonStyles from '../Button/Button.module.css';
+import styles from './Navbar.module.css';
+
+export const Navbar = () => {
+  const { pathname } = useLocation();
+
+  const expander = useRef<HTMLDivElement>(null);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const expanderHeight = expander.current?.scrollHeight || 0;
+
+  // Collapse generator list when navigating to another page
+  useEffect(() => {
+    setIsExpanded(false);
+  }, [pathname]);
+
+  const currentGenerator = generators.find(
+    (generator) => generator.route === pathname
+  );
+  const dropdownText = currentGenerator?.name || 'Choose generator';
+
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.navbarContent}>
+        <div className={styles.staticContent}>
+          <Link to="/" className={styles.homeLink}>
+            <h1 className={styles.siteName}>Jenny</h1>
+          </Link>
+
+          <button
+            className={styles.dropdown}
+            type="button"
+            onClick={() => setIsExpanded(!isExpanded)}>
+            {dropdownText} {isExpanded ? '▲' : '▼'}
+          </button>
+        </div>
+
+        <div
+          className={styles.expandableSection}
+          ref={expander}
+          style={{ maxHeight: isExpanded ? expanderHeight : 0 }}>
+          <div className={styles.spacer} />
+
+          <div className={styles.linkList}>
+            {_sortBy(generators, 'name').map((generator) => (
+              <Link
+                to={generator.route}
+                className={classNames(buttonStyles.button, styles.link)}>
+                {generator.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,5 +1,12 @@
+import { Navbar } from '../Navbar/Navbar';
+
 import styles from './PageLayout.module.css';
 
 export const PageLayout: React.FC = ({ children }) => {
-  return <div className={styles.content}>{children}</div>;
+  return (
+    <div className={styles.pageLayout}>
+      <Navbar />
+      <div className={styles.content}>{children}</div>
+    </div>
+  );
 };
