a74407091badf614f24877fe8583e679d929a809
Olivia Asch <olivia.c.asch@gmail.com>
2023-07-18 20:12:48 +0100

    Improve boykisser alt text for custom images

--- generators/Boykisser/buildAltText.ts
+++ generators/Boykisser/buildAltText.ts
@@ -2,11 +2,15 @@ import { SettingValues } from '../../types/SettingTypes';
 import { BoykisserSettings } from './types';
 
 export const buildAltText = (settings: SettingValues<BoykisserSettings>) => {
-  const { text } = settings;
+  const { text, image } = settings;
+
+  const imageDescription = image.src
+    ? "{{userImage}} with a smug face on top that's blushing"
+    : 'A line drawing of a cat making a smug face and blushing';
 
   const textDescription = text
     ? ` There's text above that says "${text}".`
     : '';
 
-  return `A line drawing of a cat making a smug face and blushing.${textDescription}`;
+  return `${imageDescription}.${textDescription}`;
 };
