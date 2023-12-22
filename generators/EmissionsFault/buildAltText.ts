dc5cfff06daf4a7e9aaaea9d0e6ebe66f5cc20b2
Liv Asch <olivia.c.asch@gmail.com>
2023-12-22 10:42:19 +0000

    Improve emissions fault alt text generation

--- generators/EmissionsFault/buildAltText.ts
+++ generators/EmissionsFault/buildAltText.ts
@@ -7,7 +7,7 @@ export const buildAltText = (
   const { text = '' } = settings;
 
   const textDescription = text.length
-    ? `a warning message that says "${text}"`
+    ? `a warning message that says "${text.replaceAll('\n', ' ').trim()}"`
     : 'a warning message with no text shown';
 
   return `The dashboard of a vehicle with a screen that has ${textDescription}. The vehicle is driving along a road at night, and out of the windscreen are the lights of other vehicles in the distance.`;
