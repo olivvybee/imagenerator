5978b0dc7c4d8b7d350645f8a14b64be80925a20
Abby Flowers <oliviaflowers93@gmail.com>
2026-09-19 19:21:01 +0100

    Add like/dislike generator

---
+++ generators/LikeDislike/constants.ts
@@ -0,0 +1,14 @@
+export const CANVAS_WIDTH = 1200;
+export const CANVAS_HEIGHT = 1000;
+
+export const DIVIDER_WIDTH = 16;
+export const TEXT_MARGIN = 16;
+
+export const FONT_SIZE = 60;
+
+export const CHARACTERS = {
+  'Hatsune Miku': { key: 'miku', pronoun: 'she' },
+  Sayori: { key: 'sayori', pronoun: 'she' },
+};
+
+export const CHARACTER_OPTIONS = Object.keys(CHARACTERS);
